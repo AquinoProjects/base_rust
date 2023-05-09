d6e53d7c87463b7f5d63293a0fc000d9ec866e06
Daniel <daniel@grupovex.com.br>
2023-05-08 21:34:13 -0300

    Changes: News types and methods

--- intro/bruno_rocha_parte5/program/src/main.rs
+++ intro/bruno_rocha_parte5/program/src/main.rs
@@ -1,6 +1,7 @@
 use std::io;
 
 fn main() {
+    println!("{:-^40}", "Calculadora");
     //let letter: &str = "Daniel"; //str
     //string slice, string reference
     
@@ -26,10 +27,23 @@ fn main() {
 
     //use import
     let mut s = String::new();
-    println!("Digite um texto");
+    let banner = "\
+    Digite uma sequêmcia de números \
+    separados por vírgula \
+    exemplo: 1,2,3,4,67\
+    ";
+    println!("{banner}");
     //Chama o io, o objeto do tipo stdin e a funcao read_line para fazer a leitura de linha no terminal
     io::stdin().read_line(&mut s).expect("Error reading console!"); // retorna um Result - contendo um valor ou um erro
-    println!("Você digitou {s}");
-    println!("Quantidade de letras {}", s.len());
+    //println!("Você digitou {}", s.to_uppercase());
+    //println!("Você digitou {}", s.replace("D", "x"));
+
+
+    //calculadora
+    let nuns: Vec<i32> = s.split(",").map(|c| c.trim().parse().expect("Error")).collect();
+    let result: i32 = nuns.iter().sum();
+    println!("O Total é:  {:?}", result);
+    println!("{}", "-".repeat(40));
+    //println!("Quantidade de letras {}", s.trim().chars().count());
 
 }
